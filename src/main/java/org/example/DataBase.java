840e74a07464ff6e116a13303ced2154479e3280
CaesarHD <stir.cezar@gmail.com>
2024-12-10 08:36:19 +0200

    Connect to java doesn't work

---
+++ src/main/java/org/example/DataBase.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBase {
+    private Connection con;
+
+    public void connect(String username, String password) throws SQLException {
+        try {
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306",
+                    username,
+                    password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+}
