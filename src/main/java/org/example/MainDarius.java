e07afff1c5399a30c96c74e776c898cd5ea22c71
POP DARIUS IOAN <popdarius1801@gmail.com>
2024-12-17 14:21:39 +0200

    conflict

--- src/main/java/org/example/MainDarius.java
+++ src/main/java/org/example/MainDarius.java
@@ -8,23 +8,14 @@ import java.sql.SQLException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MainDarius {
-    public static void dariusMain() throws SQLException {
+    public static void main(String[] args) throws SQLException {
         DataBase db = new DataBase();
         db.connect("root", "root");
 
-        Professor darius = new Professor("5050118245033", "Darius", "SefuVost", "str. Dinamo, nr. 1", "0767332424", "popdarius1801@gmail.com", "DariusIban", 6969, "profesor", db);
+        Admin darius = new Admin("5050118245033", "Darius", "SefuVost", "str. Dinamo, nr. 1", "0756992299", "popdarius1801@gmail.com", "DariusIban", 6969, "administrator",db);
+        darius.add();
 
-        UI ui = new UI();
-        JButton jButton = new JButton("new user");
-        jButton.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                darius.add();
-            }
-        });
-        ui.setLayout(new FlowLayout());
-        ui.addComponent(jButton);
-        ui.show();
+
 
     }
 }
