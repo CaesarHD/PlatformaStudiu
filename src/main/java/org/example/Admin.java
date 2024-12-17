08fa12fb28e4d942b9f899e48673b01e19b50003
POP DARIUS IOAN <popdarius1801@gmail.com>
2024-12-17 13:37:53 +0200

    Darius Main

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Admin extends User {
+    public Admin() {
+    }
+
+    public Admin(String CNP, String firstName, String secondName, String address, String phoneNumber, String email, String iban, int contractNumber, String userType, DataBase db)
+    {
+        super(CNP, firstName, secondName, address, phoneNumber, email, iban, contractNumber, userType, db);
+    }
+
+    public void deleteUser(String CNP)
+    {
+        String query = "DELETE FROM utilizatori WHERE CNP = '" + CNP + "'";
+        this.db.execute(query);
+    }
+
+    public void updateUser(String CNP,String field, String newValue)
+    {
+
+    }
+
+}
---
+++ src/main/java/org/example/MainDarius.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.sql.SQLException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MainDarius {
+    public static void main(String[] args) throws SQLException {
+        DataBase db = new DataBase();
+        db.connect("root", "root");
+
+        Professor darius = new Professor("5050118245033", "Darius", "SefuVost", "str. Dinamo, nr. 1", "0767332424", "popdarius1801@gmail.com", "DariusIban", 6969, "profesor", db);
+
+        UI ui = new UI();
+        JButton jButton = new JButton("new user");
+        jButton.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                darius.add();
+            }
+        });
+        ui.setLayout(new FlowLayout());
+        ui.addComponent(jButton);
+        ui.show();
+
+    }
+}
+}
